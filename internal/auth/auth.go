b5425ea273360db1cb41cb642df118cee2b20790
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2026-01-25 21:50:00 +0500

    Create auth.go

---
+++ internal/auth/auth.go
@@ -0,0 +1,18 @@
+package auth
+import "errors"
+type User struct {
+    ID       int
+    Username string
+    Password string // Hash
+}
+type Service struct {
+}
+func NewService() *Service {
+    return &Service{}
+}
+func (s *Service) Login(username, password string) (string, error) {
+    if username == "" || password == "" {
+        return "", errors.New("invalid credentials")
+    }
+    return "valid-jwt-token", nil
+}
